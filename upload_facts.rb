c7f9ec6ab7890e32d3b8907d3330fd5cc24c5c71
John Ericson <946327+JohnEricson@users.noreply.github.com>
2020-07-17 11:27:29 +0200

    FEAT: First version of script. Used for upload YAML-facts-files to PuppetDB. These YAML-files are generated when a Puppet Agent is executing on a node and the file is then sent to a Puppet Master. All these files are collected on the Puppet Master. This script allows you to upload these to PuppetDB using the https://puppet.com/docs/puppet/5.5/http_api/http_facts.html API. This is useful when you are migrating to a new Puppet Master, such as when you are upgrading Puppet version. Then you can just copy the yaml-files over and then upload these to PuppetDB on the new Master.

---
+++ upload_facts.rb
@@ -0,0 +1,43 @@
+#!/opt/puppetlabs/puppet/bin/ruby
+# Uploads yaml files with facts to PuppetDB. These yaml-files are
+# in the same format as when created by the Puppet Master when real
+# agents report their facts in the beginning of a Puppet Agent execution.
+#
+# Developed for copying facts from old Puppet Master to new Puppet Master 
+# when upgrading Puppet version.
+#
+# Needs to be ran as a user that has the permissions to read
+# the certificate files. Usually root on a Puppet Master.
+#
+# How to run:
+# ./upload_facts.rb /opt/puppetlabs/puppet/cache/yaml/facts/*.yaml
+# Or like this:
+# ./upload_facts.rb $(puppet config print vardir)/yaml/facts/*.yaml
+#
+# How to run when developing:
+# /opt/puppetlabs/puppet/bin/irb upload_facts.rb
+
+require 'puppet'
+require 'puppet/node/facts' # This class is required by the yaml-files.
+require 'yaml'
+require 'puppet/indirector/facts/puppetdb' # This one might be replaceable with quixoten-puppetdb-terminus.
+
+if ARGV.length < 1
+  puts 'usage: upload_facts.rb [files]'
+  puts ''
+  puts '  files:    A file or a bunch of files to upload to PuppetDB. Should be in yaml-format. Wildcards can be used.'
+  puts ''
+  exit 1
+end
+
+Puppet.initialize_settings
+puppetdb = Puppet::Node::Facts::Puppetdb.new
+
+ARGV.each do |file|
+  puts "Uploading facts from file: #{file}"
+	facts = YAML.load_file(file)
+	options = {}
+	request = Puppet::Node::Facts.indirection.request(:save, facts.name, facts, options)
+	puppetdb.save(request)
+end
+
